a600d2d87212d4f5d1ae20acd8c8445ba8008c23
dwaghray <dwaghray@umail.iu.edu>
2017-12-23 14:22:43 -0500

    Add files via upload

---
+++ 5.printRange.py
@@ -0,0 +1,16 @@
+from tools import *
+
+# This file imports from tools.py and only contains the main code
+# for the print function. This program takes three values; low,
+# high, and factor. It goes through all integers in the range and
+# determines if each one is divisible by the factor, and prints a
+# message indicating the divisible values.
+
+
+#main
+
+print_range(2, 10, 3)
+
+print()
+
+print_range(1, 50, 15)
